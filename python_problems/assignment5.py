99ac3e89b68527489093a7a8f7ecc31b3da44a59
shq749 <106631675+shq749@users.noreply.github.com>
2022-05-31 18:43:01 -0400

    Add files via upload

---
+++ python_problems/assignment5.py
@@ -0,0 +1,300 @@
+from typing import List
+from functools import reduce
+import sys
+import traceback
+
+#################
+### Problem  1 ##
+#################
+
+def closest_to (l, v):
+    a=0
+    b=0
+    c=0
+    if l==[]:
+        return None
+    else:
+        d=0
+        for i in l:
+            a=abs(int(l[c])-int(v))
+            if c==0:
+                b=a
+                d=l[c]
+                c=c+1
+            elif b>a:
+                b=a
+                d=l[c]
+                c=c+1
+            else:
+                c=c+1
+    return d
+
+
+
+
+#################
+### problem  2 ##
+#################
+
+def assoc_list (l):
+    if l==[]:
+        return []
+    else:
+        l=sorted(l)
+        l1=l
+        l2=[]
+        count=0
+        for i in l:
+            for n in l1:
+                if i==n:
+                    count=count+1
+            l2.append((i,count))
+            count=0
+        l2=list(set(l2))
+        return l2
+
+#################
+### Problem  3 ##
+#################
+
+def buckets (f, l):
+    if l==[]:
+        return [] 
+    else:
+        A=l
+        lis=[]
+        lis1=[]
+        for i in l:
+            for n in A:
+                if f(i,n):
+                    lis.append(n)
+                else:
+                    pass
+            lis1.append(lis)
+            lis=[]
+        lis1=list(map(list,set(map(tuple,lis1))))
+        lis1=sorted(lis1, key=lambda x:x[0])
+        return lis1
+
+
+###################################
+# Definition for a binary tree node
+###################################
+
+class TreeNode:
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    # construct tree from a list of values `ls`
+    def list_to_tree(self, ls):
+        self.val = self.left = self.right = None # clear the current tree
+
+        if not ls: # ls is None or l == []
+            return # tree is None
+
+        i = 0
+        self.val = ls[i]
+        queue = [self]
+        while queue: # while queue is not empty
+            i += 1
+            node = queue.pop(0)
+            if node.val is None:
+                continue
+
+            if 2*i -1 >= len(ls) or ls[2*i-1] is None:
+                pass
+            else:
+                node.left = TreeNode(ls[2*i-1])
+                queue.append(node.left)
+
+            if 2*i >= len(ls) or ls[2*i] is None:
+                pass
+            else:
+                node.right = TreeNode(ls[2*i])
+                queue.append(node.right)
+
+
+#################
+### Problem  4 ##
+#################
+
+def level_order(root: TreeNode):
+    if root== []:
+        return []
+    else:
+        rlevel=[]
+        rvalue=[root.val]
+        queue=[root]
+        while queue:
+            rlevel.append(rvalue)
+            rvalue=[]
+            #print('out',root.val)
+            for i in range(len(queue)):
+                root=queue.pop(0)
+                if root.left!=None:
+                    queue.append(root.left)
+                    rvalue.append(root.left.val)
+                else:
+                    pass
+                if root.right!=None:
+                    queue.append(root.right)
+                    rvalue.append(root.right.val)
+                else:
+                    pass
+                
+    return rlevel  
+
+
+#################
+### Problem  5 ##
+#################
+
+def path(root):
+    res=[]
+    queue=[root]
+    if root is None:
+        return[]
+    if root.left==None and root.right==None:
+        return [str(root.val)]
+    else:
+        paths=path(root.left)+path(root.right)
+        #print(paths)
+        for i in paths:
+        #str so that can seperate
+            res.append(str(root.val)+','+i)
+        return res
+
+def sum(lst, num):
+    res=0
+    resl=[]
+    level=[]
+    if lst==[]:
+        return []
+    for i in lst:
+        i=i.split(",")
+        for j in i:
+            j=int(j)
+            level.append(j)
+            res=res+j
+        if res==num:
+            resl.append(level)
+        level=[]
+        res=0
+    return resl
+
+def pathSum(root: TreeNode, targetSum: int) -> List[List[int]]:
+    c=path(root)
+    res=sum(c, targetSum)
+    return res
+
+
+
+
+#################
+### Test cases ##
+#################
+
+def main():
+    print ("Testing your code ...")
+    error_count = 0
+
+    # Testcases for Problem 1
+    try:
+        assert (closest_to([2,4,8,9],7) == 8)
+        assert (closest_to([2,3,7,9],5) == 3)
+    except AssertionError as err:
+        error_count += 1
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+        # tb_info = traceback.extract_tb(tb)
+        # filename, line, func, text = tb_info[-1]
+        # print('An error occurred on line {} in statement {}'.format(line, text))
+    except:
+        error_count += 1
+        print("Unexpected error:", sys.exc_info()[0])
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+
+    # Testcases for Problem 2
+    try:
+        result = assoc_list([1, 2, 2, 1, 3])
+        result.sort(key=lambda x:x[0])
+        assert (result == [(1,2), (2, 2), (3, 1)])
+
+        result = assoc_list(["a","a","b","a"])
+        result.sort(key=lambda x:x[0])
+        assert (result == [("a",3), ("b",1)])
+
+        result = assoc_list([1, 7, 7, 1, 5, 2, 7, 7])
+        result.sort(key=lambda x:x[0])
+        assert (result == [(1,2), (2,1), (5,1), (7,4)])
+    except AssertionError as err:
+        error_count += 1
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+    except:
+        error_count += 1
+        print("Unexpected error:", sys.exc_info()[0])
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+
+    # Testcases for Problem 3
+    try:
+        assert (buckets (lambda a, b : a == b, [1,2,3,4]) == [[1], [2], [3], [4]])
+        assert (buckets (lambda a, b : a == b, [1,2,3,4,2,3,4,3,4]) == [[1], [2, 2], [3, 3, 3], [4, 4, 4]])
+        assert (buckets (lambda a, b : a % 3 == b % 3, [1,2,3,4,5,6]) == [[1, 4], [2, 5], [3, 6]])
+    except AssertionError as err:
+        error_count += 1
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+    except:
+        error_count += 1
+        print("Unexpected error:", sys.exc_info()[0])
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+
+    ### Specify 3 trees for testing problems 4 & 5
+    root_1 = TreeNode()
+    root_1.list_to_tree([5,4,8,11,None,13,4,7,2,None,None,5,1])
+
+    root_2 = TreeNode()
+    root_2.list_to_tree([1,2,3])
+
+    root_3 = TreeNode()
+    root_3.list_to_tree([1,2])
+
+    # Testcases for Problem 4
+    try:
+        assert (level_order(root_1) == [[5], [4, 8], [11, 13, 4], [7, 2, 5, 1]])
+        assert (level_order(root_2) == [[1], [2, 3]])
+        assert (level_order(root_3) == [[1], [2]])
+    except AssertionError as err:
+        error_count += 1
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+    except:
+        error_count += 1
+        print("Unexpected error:", sys.exc_info()[0])
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+
+    # Testcases for Problem 5
+    try:
+        assert (pathSum(root_1, 22) == [[5, 4, 11, 2], [5, 8, 4, 5]])
+        assert (pathSum(root_2, 4) == [[1, 3]])
+        assert (pathSum(root_3, 0) == [])
+    except AssertionError as err:
+        error_count += 1
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+    except:
+        error_count += 1
+        print("Unexpected error:", sys.exc_info()[0])
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb)
+
+    print (f"{error_count} out of 5 programming questions are incorrect.")
+
+main()
